098c2cca1593cf6dac8099e505d90ec56918705d
Lazzatios <lseiilova@nu.edu.kz>
2021-07-03 16:53:48 +0600

    Initial Commit

---
+++ task7/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  task7
+//
+//  Created by Lazzat Seiilova on 03.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ task7/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  task7
+//
+//  Created by Lazzat Seiilova on 03.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
